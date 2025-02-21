c29d6cad1b9320a1a345c5a36bbd2da8580e9c07
henriqueyun <henriqueyun@gmail.com>
2025-02-21 17:36:43 -0300

    feat: added drawer

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -1,7 +1,16 @@
 <template>
-    <v-app-bar class="pa-2">
+    <v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer"/>
         <img height="100%" src="../assets/grupo-a-logo.png"/>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer">
+      <v-list-item title="Módulo Academico"/>
+        <v-divider/>
+      <v-list-item link title="Alunos"/>
+    </v-navigation-drawer>
 </template>
-<script>
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const drawer = ref(false)
 </script>
